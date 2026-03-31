5036ba438f202dfd404f3a62bada15ccb6a46b7e
SPareshKumar <s.paresh2005@gmail.com>
2026-03-31 23:25:06 +0530

    remove posthog

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,8 +2,6 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { Toaster } from 'sonner';
-import { CSPostHogProvider } from './providers'
-import SuspendedPostHogPageView from './PostHogPageView'
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -20,12 +18,9 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${inter.className} bg-ipl-bg text-white min-h-screen`}>
-        <CSPostHogProvider>
-          <SuspendedPostHogPageView />
           {children}
           {/* This enables the red toast notifications anywhere in the app */}
           <Toaster theme="dark" position="top-center" richColors />
-        </CSPostHogProvider>
       </body>
     </html>
   );
